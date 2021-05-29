aa75ec6b1af04e223f20e196f61f07b95464f429
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-05-29 11:28:51 -0400

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+focusHandler = () => {
+    console.log('Good!')
+}
+
+blurHandler = () => {
+    console.log('Hey! Eyes on me!')
+}
+
+    render() {
+        return (
+            <div>
+                <EyesOnMe />
+                <button onFocus={this.focusHandler} onBlur={this.blurHandler}></button>
+            </div>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+
+    passwordHandler = () => {
+        console.log('Entering password...');
+    }
+
+    render() {
+        return (
+            <div>
+                <Keypad />
+                <input onKeyUp={this.passwordHandler} type="password"></input>
+            </div>
+        )
+    }
+}
